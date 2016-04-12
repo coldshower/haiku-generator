65ce548a1e06b3557d45f4b96760fbded2960c31
John Chen <john@johnchen.co>
2016-04-12 01:27:15 -0400

    first working iteration

--- createHaiku.js
+++ createHaiku.js
@@ -1,17 +1,46 @@
-function createHaiku(structure_arr, probability_arr) { // format [5, 7, 5]
-	function createLine(numSyll) {
-		var currSyll = 0;
-
-		while (currSyll < numSyll) {
-
-		}
-
+function createHaiku(structure_arr, probability_arr, formattedData) { // format for structure_arr: [5, 7, 5]
+	function getRandomNumber(max) {
+		return Math.floor(Math.random() * (max + 1));
 	}
+
+	function syllableToUse(random_num) {
+		for (var i = 0, len = probability_arr.length; i < len; i++) {
+			if (probability_arr[i] >= random_num) {
+					return i;
+				}
+		}
+	}
+
+	function createLine(numSyll) {
+		var currentSyll = 0;
+		var line = "";
+
+		while (currentSyll < numSyll) {
+			var syllables_left = numSyll - currentSyll;
+			var random = getRandomNumber(probability_arr[syllables_left]);
+			var syllable_key = syllableToUse(random);	
+			// console.log(random);
+			// console.log(probability_arr);
+			// console.log(syllable_key);
+
+			var chosen_word_index = getRandomNumber(formattedData[syllable_key].length);
+			var chosen_word = formattedData[syllable_key][chosen_word_index].word;
+			
+			line += chosen_word + " ";
+
+			currentSyll += +syllable_key;
+		}
+		return line.slice(0, -1);
+	}
+
 	var haiku = '';
 
-	structure.forEach(function(syllables) {
+	structure_arr.forEach(function(syllables) {
 		haiku += createLine(syllables);
+		haiku += "\n";
 	});
 
 	return haiku;
 }
+
+module.exports = createHaiku;
--- haiku.js
+++ haiku.js
@@ -8,6 +8,8 @@ var cmuDict = fs.readFileSync('./cmudict.txt', 'utf-8');
 
 var formattedData = formatData(cmuDict);
 
-console.log(lengthCalculate(formattedData))
+var probability_arr = lengthCalculate(formattedData);
 
-console.log(formattedData['0']);
+var haiku = createHaiku([5,7,5], probability_arr, formattedData);
+
+console.log(haiku);
